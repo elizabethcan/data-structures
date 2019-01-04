b42899524ec662ad710fbd6e9a05521861bea916
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-04 15:48:47 -0800

    implements functionality for Stack.push under functional instantiation

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -7,12 +7,22 @@ var Stack = function() {
   // Implement the methods below
   someInstance.push = function(value) {
     // if object is empty, set the key to the current length (0) and the value to the input value
-    storage[storage.size] = value;
+    if (storage.size === 0) { 
+      storage[storage.size] = value;
+    } else {
     // for the second item we must first add a new key (can be set to anything to start)
     // now set object[size - 1] to the value from the object[size - 2]
     // next set object[size - 2] to the value from object[size - 3]
     // repeat this while (size - i) > 0
-    // then do base case for object[0] = value 
+      storage[storage.size] = 'placeholder'
+      var i = 1;
+      while ((storage.size - i) > 1) {
+        storage[storage.size - i] = storage[storage.size - i - 1]
+        i--;
+      } 
+      // then do base case for object[0] = value
+      storage[0] = value;
+    }
   };
 
   someInstance.pop = function() {
