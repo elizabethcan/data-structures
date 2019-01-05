815ca348e465084aa3a281554cba45facae42373
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-05 13:53:36 -0800

    fixed syntax error in queue functional shared. all tests passing for functional shared now

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -9,20 +9,20 @@ var Queue = function() {
 
 var queueMethods = {};
 
-queueMehthods.enqueue = function(value) {
-    this.storage[Object.keys(this.storage).length] = value;
+queueMethods.enqueue = function(value) {
+  this.storage[Object.keys(this.storage).length] = value;
 }
 
 queueMethods.dequeue = function() {
-    var dequeued = this.storage[0];
-    delete this.storage.storage[0];
-    for (var i = 0; i < Object.keys(this.storage).length; i++) {
-        this.storage[i] = this.storage[i + 1];
-    }
-    delete this.storage[Object.keys(this.storage).length - 1];
-    return dequeued;
+  var dequeued = this.storage[0];
+  delete this.storage[0];
+  for (var i = 0; i < Object.keys(this.storage).length; i++) {
+      this.storage[i] = this.storage[i + 1];
+  }
+  delete this.storage[Object.keys(this.storage).length - 1];
+  return dequeued;
 }
 
 queueMethods.size = function() {
-    return Object.keys(this.storage).length;
+  return Object.keys(this.storage).length;
 }
