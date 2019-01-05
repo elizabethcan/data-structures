a72585a25fb34b30c68d4a5f5d15103b1089b026
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-05 13:44:07 -0800

    adjusted indentation issue in stack functional shared and added this implementation to queue but tests are not passing

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -3,7 +3,6 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
   someInstance.storage = {}
-  //var storage = {};
   _.extend(someInstance, queueMethods);
   return someInstance;
 };
@@ -11,19 +10,19 @@ var Queue = function() {
 var queueMethods = {};
 
 queueMehthods.enqueue = function(value) {
-    this[Object.keys(this).length] = value;
+    this.storage[Object.keys(this.storage).length] = value;
 }
 
 queueMethods.dequeue = function() {
-    var dequeued = this[0];
-    delete this[0];
-    for (var i = 0; i < Object.keys(this).length; i++) {
-        this[i] = this[i + 1];
+    var dequeued = this.storage[0];
+    delete this.storage.storage[0];
+    for (var i = 0; i < Object.keys(this.storage).length; i++) {
+        this.storage[i] = this.storage[i + 1];
     }
-    delete this[Object.keys(this).length - 1];
+    delete this.storage[Object.keys(this.storage).length - 1];
     return dequeued;
 }
 
 queueMethods.size = function() {
-    return Object.keys(this).length;
+    return Object.keys(this.storage).length;
 }
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,8 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-var someInstance = {};
-someInstance.storage = {};
+  var someInstance = {};
+  someInstance.storage = {};
   _.extend(someInstance, stackMethods);
   return someInstance;
 };
