03f9b03f43c042994510870c9fcdd22cf9425417
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-05 16:38:55 -0800

    finished implementing es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,25 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  enqueue(value) {
+    this.storage[Object.keys(this.storage).length] = value;
+  }
+
+  dequeue() {
+    var dequeued = this.storage[0];
+    delete this.storage[0];
+    for (var i = 0; i < Object.keys(this.storage).length; i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+    delete this.storage[Object.keys(this.storage).length - 1];
+    return dequeued;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
